8c2115b2e5ec82aaa60383bc78d91fb9e3e735f2
NikKowPHP <nik.kow@outlook.com>
2025-08-12 14:55:27 +0200

    remove trafic lights

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -66,12 +66,7 @@ const DialogContent = React.forwardRef<
       )}
       {...props}
     >
-      {/* macOS traffic lights for desktop */}
-      <div className="absolute top-4 left-4 hidden sm:flex items-center gap-2">
-        <div className="h-3 w-3 rounded-full bg-[#ff5f57]"></div>
-        <div className="h-3 w-3 rounded-full bg-[#febb2e]"></div>
-        <div className="h-3 w-3 rounded-full bg-[#28c840]"></div>
-      </div>
+     
       {children}
       {showCloseButton && (
         <DialogPrimitive.Close
